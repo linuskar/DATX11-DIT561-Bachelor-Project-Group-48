2da36d964484a01db28cd2789e2130f37b872c3d
Linus Karlsson <linuskungalv@gmail.com>
2025-02-25 21:45:32 +0100

    Added function to place a building

---
+++ building.gd
@@ -0,0 +1,5 @@
+class_name Building
+extends Area2D
+
+@export var building: Sprite2D
+@export var grid_size: int = 32
---
+++ test_placeable_level.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+var is_building: bool 
+
+signal build_mode(is_building)
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	is_building = false
+	
+	# build_mode.connect(_on_build_mode)
+	# build_mode.emit(is_building)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _input(event):
+	if event.is_action_pressed("build") or (is_building and event.is_action_pressed("place")):
+		is_building = !is_building
+		build_mode.emit(is_building)
