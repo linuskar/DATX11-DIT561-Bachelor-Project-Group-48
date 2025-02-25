4bc52f6aec428c19045a9eac6703b2af4c19ee7b
Linus Karlsson <linuskungalv@gmail.com>
2025-02-25 21:45:32 +0100

    Added function to place a building

---
+++ building_manager.gd
@@ -0,0 +1,50 @@
+extends Node
+
+@export var blueprint: Building
+@export var grid_size: int = 32
+@onready var building_layer: TileMapLayer = $"../BuildingLayer"
+var factory = preload("res://building.tscn")
+
+var buildings_placed: Array[Building]
+
+var in_build_mode: bool = false
+
+func _ready() -> void:
+	$"..".build_mode.connect(_on_build_mode)
+
+func _process(delta):
+	if in_build_mode:
+		var tile_pos = building_layer.local_to_map(get_parent().get_local_mouse_position())
+		var world_pos = building_layer.map_to_local(tile_pos)
+		
+		blueprint.position = world_pos 
+
+func _input(event):
+	var tile_pos = building_layer.local_to_map(get_parent().get_local_mouse_position())
+	var world_pos = building_layer.map_to_local(tile_pos)
+		
+	if in_build_mode and event.is_action_pressed("place"):
+		place_building()
+
+func snap_to_grid(pos, grid_size):
+	return Vector2(round(pos.x / grid_size) * grid_size, round(pos.y / grid_size) * grid_size)
+
+func place_building():
+	in_build_mode = false
+	
+	var new_building: Building = factory.instantiate()
+	
+	new_building.position = blueprint.position
+	
+	buildings_placed.append(new_building)
+	$"..".add_child(new_building)
+	$"..".build_mode.emit(in_build_mode)
+	print(buildings_placed.size())
+	
+func _on_build_mode(is_building) -> void:
+	in_build_mode = is_building
+	
+	if(in_build_mode):
+		blueprint.show()
+	else:
+		blueprint.hide()  
