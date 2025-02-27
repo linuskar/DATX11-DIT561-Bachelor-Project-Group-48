60470874021b73f83d0e1495db6977e6c331ae0c
Simbpa <113666561+Simbpa@users.noreply.github.com>
2025-02-27 15:33:29 +0100

    added comments and deleted a script attachment that doesn't exist

--- scripts/camera_movement.gd
+++ scripts/camera_movement.gd
@@ -4,12 +4,19 @@ extends Camera2D
  
 var zoomTarget :Vector2
 
-var simplePanSpeed = 4
-var maxZoom = 3.6
-var minZoom = 0.7
+#Decide how fast wasd pan speed is, higher nr is faster
+@export var simplePanSpeed = 4
+
+#Decide max and min zoom
+#Bigger nr = more zoomed in
+@export var maxZoom = 3.6
+@export var minZoom = 0.7
 
 var maxZoomLength = Vector2(maxZoom, maxZoom).length()
 var minZoomLength = Vector2(minZoom, minZoom).length()
+
+#Decide how sensitive the zoom is, bigger number is more sensitiv
+@export var zoomSensitivity = 0.1
 
 var dragStartMousePos = Vector2.ZERO
 var dragStartCameraPos = Vector2.ZERO
@@ -24,22 +31,25 @@ func _process(delta):
 	SimplePan()
 	ClickAndDrag()
 
+#Code for zoom
 func Zoom(delta):
 	if Input.is_action_just_pressed("camera_zoom_in"):
-		if (zoomTarget * 1.1).length() < maxZoomLength:
-			zoomTarget *= 1.1
+		if (zoomTarget * (1 + zoomSensitivity)).length() < maxZoomLength:
+			zoomTarget *= 1 + zoomSensitivity
 		else:
 			zoomTarget = Vector2(maxZoom, maxZoom)
 		print(zoomTarget)
 	
 	if Input.is_action_just_pressed("camera_zoom_out"):
-		if (zoomTarget * 0.9).length() > minZoomLength:
-			zoomTarget *= 0.9
+		if (zoomTarget * (1 - zoomSensitivity)).length() > minZoomLength:
+			zoomTarget *= 1 - zoomSensitivity
 		else:
 			zoomTarget = Vector2(minZoom, minZoom)
 	
 	zoom = zoom.slerp(zoomTarget, zoomSpeed * delta)
 
+#Code for wasd movement
+#Can change/add movment buttons in Project -> Project settings -> Input map
 func SimplePan():
 	if Input.is_action_pressed("camera_move_right"):
 		position.x += simplePanSpeed
@@ -53,6 +63,8 @@ func SimplePan():
 	if Input.is_action_pressed("camera_move_down"):
 		position.y += simplePanSpeed
 
+#Code for the click and drag movement
+#Can change pan button in Project -> Project settings -> Input map
 func ClickAndDrag():
 	if !isDragging and Input.is_action_just_pressed("camera_pan"):
 		dragStartMousePos = get_viewport().get_mouse_position()
