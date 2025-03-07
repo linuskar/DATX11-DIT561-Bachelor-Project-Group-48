01a73409fa212c4412d0add058b193e68e656557
Simbpa <113666561+Simbpa@users.noreply.github.com>
2025-03-07 15:08:52 +0100

    Added signal for resources and coal miner scene

---
+++ scripts/buildings/coal_miner.gd
@@ -0,0 +1,35 @@
+extends StaticBody2D
+
+##The max amount of resources the building can keep
+@export var maxStorageCoal = 20
+
+##How much coal is generated each cycle, cyclespeed is timer wait time
+@export var coalGenerated = 5
+
+##How much co2 emits per cycle, not saved in the building
+@export var co2Emission = 5
+
+var currentStorageCoal = 0
+
+func _ready():
+	#Connect the signal that can take resources from this building
+	ResourceSignals.get_resource.connect(_send_resources)
+
+#Activated at the end of each cycle
+func _on_timer_timeout() -> void:
+	add_resources()
+
+#Add the resources to the storage and emit what have been created
+func add_resources():
+	ResourceSignals.add_resource.emit("co2", co2Emission)
+	if currentStorageCoal + coalGenerated <= maxStorageCoal:
+		currentStorageCoal += coalGenerated
+		ResourceSignals.add_resource.emit("Coal", coalGenerated)
+	else:
+		$Timer.stop()
+
+#Take resources from this buildings storage
+func _send_resources(resource_type, amount):
+	if resource_type == "Coal":
+		currentStorageCoal -= amount
+		$Timer.autostart()
