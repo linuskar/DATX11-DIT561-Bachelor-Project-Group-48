0d85aba61b340f141702963d2b1fcb85c9e61dab
Simbpa <113666561+Simbpa@users.noreply.github.com>
2025-03-07 15:28:33 +0100

    added wood cutter and iron mine

---
+++ scripts/buildings/iron_miner.gd
@@ -0,0 +1,35 @@
+extends StaticBody2D
+
+##The max amount of resources the building can keep
+@export var maxStorageIron = 20
+
+##How much iron is generated each cycle, cyclespeed is timer wait time
+@export var ironGenerated = 5
+
+##How much co2 emits per cycle, not saved in the building
+@export var co2Emission = 5
+
+var currentStorageIron= 0
+
+func _ready():
+	#Connect the signal that can take resources from this building
+	ResourceSignals.get_resource.connect(_send_resources)
+
+#Activated at the end of each cycle
+func _on_timer_timeout() -> void:
+	add_resources()
+
+#Add the resources to the storage and emit what have been created
+func add_resources():
+	ResourceSignals.add_resource.emit("co2", co2Emission)
+	if currentStorageIron + ironGenerated <= maxStorageIron:
+		currentStorageIron += ironGenerated
+		ResourceSignals.add_resource.emit("Iron", ironGenerated)
+	else:
+		$Timer.stop()
+
+#Take resources from this buildings storage
+func _send_resources(resource_type, amount):
+	if resource_type == "Iron":
+		currentStorageIron -= amount
+		$Timer.autostart()
---
+++ scripts/buildings/wood_cutter.gd
@@ -0,0 +1,36 @@
+extends StaticBody2D
+
+##The max amount of resources the building can keep
+@export var maxStorageWood = 20
+
+##How much iron is generated each cycle, cyclespeed is timer wait time
+@export var woodGenerated = 5
+
+##How much co2 emits per cycle, not saved in the building
+@export var co2Emission = 5
+
+var currentStorageWood= 0
+
+func _ready():
+	#Connect the signal that can take resources from this building
+	ResourceSignals.get_resource.connect(_send_resources)
+
+#Activated at the end of each cycle
+func _on_timer_timeout() -> void:
+	add_resources()
+	print(currentStorageWood)
+
+#Add the resources to the storage and emit what have been created
+func add_resources():
+	ResourceSignals.add_resource.emit("co2", co2Emission)
+	if currentStorageWood + woodGenerated <= maxStorageWood:
+		currentStorageWood += woodGenerated
+		ResourceSignals.add_resource.emit("Wood trunk", woodGenerated)
+	else:
+		$Timer.stop()
+
+#Take resources from this buildings storage
+func _send_resources(resource_type, amount):
+	if resource_type == "Wood trunk":
+		currentStorageWood -= amount
+		$Timer.autostart()
