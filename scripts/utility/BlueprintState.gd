4043d3fb574e3107b1a0d16ccc65008646cc654b
Heldereim <adam.hellgard@hotmail.se>
2025-03-11 13:48:28 +0100

    Updates made towards statechanging

---
+++ scripts/utility/BlueprintState.gd
@@ -0,0 +1,10 @@
+class_name BlueprintState extends FactoryState
+
+func enter() -> void:
+	pass
+
+func exit() -> void:
+	pass
+
+func update(_delta) -> void:
+	pass
---
+++ scripts/utility/State.gd
@@ -0,0 +1,10 @@
+class_name FactoryState extends Node
+
+func enter() -> void:
+	pass
+	
+func exit() -> void:
+	pass
+	
+func update(_delta) -> void:
+	pass
