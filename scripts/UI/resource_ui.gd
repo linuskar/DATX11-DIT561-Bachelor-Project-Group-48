91c00267e2494f570427ba8c05c3542617477808
Kevin Faghihi <kevinfaghihi@hotmail.com>
2025-03-16 11:25:57 +0100

    Skapat UI node, och Resource UI som child node med ett påbörjat script

---
+++ scripts/UI/resource_ui.gd
@@ -0,0 +1,14 @@
+extends Panel
+
+var resource_labels = {}
+
+func _ready():
+	# Setup references to each resource label
+	resource_labels[Enums.ResourceType.WOOD] = $HBoxContainer/Wood
+
+	# Connect to the global signal
+	ResourceSignals.update_UI.connect(_on_update_UI)
+
+func _on_update_UI(resource_type: Enums.ResourceType, amount: int) -> void:
+	if resource_type in resource_labels:
+		resource_labels[resource_type].text = str(amount)
