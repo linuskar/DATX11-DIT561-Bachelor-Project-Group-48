10246c2cff328491173ae5a8cabdc8598c0129bf
Heldereim <adam.hellgard@hotmail.se>
2025-03-26 21:34:36 +0100

    Building info panel updates
    More work done on the building info panel. The panel now displays inputs, outputs, max storage as well as the tiles it can be placed on

--- scripts/UI/BuildingInfo.gd
+++ scripts/UI/BuildingInfo.gd
@@ -4,6 +4,8 @@ extends Control
 @onready var building_name = $ScrollContainer/Information/BuildingName
 @onready var info = $ScrollContainer/Information/BuildingInfo
 
+var show_emissions = false
+
 func _ready() -> void:
 	BuildingSignals.building_clicked.connect(set_active)
 	set_inactive()
@@ -14,11 +16,32 @@ func populate_info_label(building: Building) -> void:
 	building_name.set_text(Enums.building_type_to_string(building.building_data.building_type))
 	info.text = get_text(building.building_data)
 
-
+## Formating building data into a string that is then displayed in the
+## building info panel.
 func get_text(building_data: ProductionBuildingData) -> String:
 	var text: String = ""
-	for key in building_data.output_generation:
-		text += Enums.resource_type_to_string(key) + '\n'
+	
+	## Adds all the different outputs of the building
+	text += "\nOutputs\n"
+	for key in building_data.output_generation.keys():
+		text += Enums.resource_type_to_string(key) + ': ' + str(building_data.output_generation.get(key)) + '\n'
+
+	## Adds all the different inputs of the building
+	text += "\nInputs\n"
+	for key in building_data.input_use_rates.keys():
+		text += Enums.resource_type_to_string(key) + ': ' + str(building_data.input_use_rates.get(key)) + '\n'
+	
+	## Adds valid placement tiles to the text
+	text += "\nPlacement\n"
+	for tile in building_data.valid_tile_types_to_place_on:
+		text += Enums.tile_type_to_string(tile) + '\n'
+	
+	## Adds storage capacity to the text
+	text += "\nStorage\n"
+	for key in building_data.max_storage.keys():
+		text += Enums.resource_type_to_string(key) + ': ' + str(building_data.max_storage.get(key)) + '\n'
+	
+	
 	return text
 
 func set_inactive() -> void:
