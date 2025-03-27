1cf66a2d5fd7218f062b35f11680002f93dd4772
Linus Karlsson <linuskungalv@gmail.com>
2025-03-27 15:30:21 +0100

    Refactored code

--- scripts/utility/map_areas.gd
+++ scripts/utility/map_areas.gd
@@ -28,18 +28,15 @@ func _ready():
 	playable_area.connect("mouse_entered", _on_mouse_entered)
 	playable_area.connect("mouse_exited", _on_mouse_exited)
 	
-	left_bound.connect("area_entered", _on_blueprint_entered_left_bound)
-	left_bound.connect("area_exited", _on_blueprint_exited_left_bound)
-	
-	right_bound.connect("area_entered", _on_blueprint_entered_right_bound)
-	right_bound.connect("area_exited", _on_blueprint_exited_right_bound)
-	
-	lower_bound.connect("area_entered", _on_blueprint_entered_lower_bound)
-	lower_bound.connect("area_exited", _on_blueprint_exited_lower_bound)
-	
-	upper_bound.connect("area_entered", _on_blueprint_entered_upper_bound)
-	upper_bound.connect("area_exited", _on_blueprint_exited_upper_bound)
-	
+	playable_area.connect("area_entered", _on_playable_area_entered)
+	playable_area.connect("area_exited", _on_playable_area_exited)
+
+func _on_playable_area_entered() -> void:
+	blueprint_in_map.emit(true)
+
+func _on_playable_area_exited() -> void:
+	blueprint_in_map.emit(false)
+
 ## Function for when the mouse enters the map bounds
 func _on_mouse_entered() -> void:
 	mouse_in_map.emit(true)
@@ -47,38 +44,3 @@ func _on_mouse_entered() -> void:
 ## Function for when the mouse exits the map bounds
 func _on_mouse_exited() -> void:
 	mouse_in_map.emit(false)
-
-func is_blueprint_in_map() -> bool:
-	return not in_left_bound and not in_right_bound and not in_lower_bound and not in_upper_bound
-
-func _on_blueprint_entered_left_bound(area: Area2D) -> void:
-	in_left_bound = true
-	blueprint_in_map.emit(is_blueprint_in_map())
-
-func _on_blueprint_exited_left_bound(area: Area2D) -> void:
-	in_left_bound = false
-	blueprint_in_map.emit(is_blueprint_in_map())
-
-func _on_blueprint_entered_right_bound(area: Area2D) -> void:
-	in_right_bound = true
-	blueprint_in_map.emit(is_blueprint_in_map())
-
-func _on_blueprint_exited_right_bound(area: Area2D) -> void:
-	in_right_bound = false
-	blueprint_in_map.emit(is_blueprint_in_map())
-
-func _on_blueprint_entered_lower_bound(area: Area2D) -> void:
-	in_lower_bound = true
-	blueprint_in_map.emit(is_blueprint_in_map())
-
-func _on_blueprint_exited_lower_bound(area: Area2D) -> void:
-	in_lower_bound = false
-	blueprint_in_map.emit(is_blueprint_in_map())
-
-func _on_blueprint_entered_upper_bound(area: Area2D) -> void:
-	in_upper_bound = true
-	blueprint_in_map.emit(is_blueprint_in_map())
-
-func _on_blueprint_exited_upper_bound(area: Area2D) -> void:
-	in_upper_bound = false
-	blueprint_in_map.emit(is_blueprint_in_map())
