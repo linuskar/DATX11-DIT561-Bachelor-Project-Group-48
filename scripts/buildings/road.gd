4f22dfcb6d1b6479ef098f889084997c805d0a94
Ludvigot <ludvigotterlind@gmail.com>
2025-03-29 13:22:03 +0100

    Fixed the road sprite
    The road displays the correct sprite when placed

--- scripts/buildings/road.gd
+++ scripts/buildings/road.gd
@@ -1,23 +1,50 @@
 class_name Road
 extends ProductionBuilding
-## A class that is for the aspects of a coal power plant.
-##
-## A class that is for the aspects of a coal power plant. This class extends from
-## the ProductionBuilding class.
-##
 
 static var road_positions: Array[Vector2] = []
 
-
+var left: bool = false
+var right: bool = false
+var up: bool = false
+var down: bool = false
 
 func _ready():
 	road_positions.append(position)
 	super()
-	## Test if power generates
-	## input_storage.set(Enums.ResourceType.COAL, 100)
-	
+	update_connections()
+
 func _process(delta: float) -> void:
-	if road_positions.has(position + Vector2(32,0)) or road_positions.has(position - Vector2(32,0)):
-		$Sprite2D.frame = 4
-	if road_positions.has(position + Vector2(0,32)) or road_positions.has(position - Vector2(32,0)):
-		$Sprite2D.frame = 4
+	update_connections()
+	
+	if up and down and left and right:
+		$Sprite2D.frame = 2  # Fully connected crossroad
+	elif up and left and down:
+		$Sprite2D.frame = 1  # T-junction (right open)
+	elif up and right and down:
+		$Sprite2D.frame = 7  # T-junction (left open)
+	elif left and right and down:
+		$Sprite2D.frame = 10  # T-junction (top open)
+	elif left and right and up:
+		$Sprite2D.frame = 11  # T-junction (bottom open)
+	elif left and right:
+		$Sprite2D.frame = 4  # Horizontal road
+	elif up and down:
+		$Sprite2D.frame = 3  # Vertical road
+	elif right and down:
+		$Sprite2D.frame = 5  # Top-left corner
+	elif left and down:
+		$Sprite2D.frame = 6  # Top-right corner
+	elif right and up:
+		$Sprite2D.frame = 8  # Bottom-left corner
+	elif left and up:
+		$Sprite2D.frame = 9  # Bottom-right corner
+	elif left or right:
+		$Sprite2D.frame = 4  # Default horizontal
+	elif up or down:
+		$Sprite2D.frame = 3  # Default vertical
+
+func update_connections():
+	left = road_positions.has(position + Vector2(-32, 0))
+	right = road_positions.has(position + Vector2(32, 0))
+	up = road_positions.has(position + Vector2(0, -32))
+	down = road_positions.has(position + Vector2(0, 32))
