c05a47579dfe940aef9f9e0979d0f56f448f1c70
Linus Karlsson <linuskungalv@gmail.com>
2025-04-04 13:48:07 +0200

    Worked on biomass overflow

--- scripts/buildings/gathering_buildings/wood_cutter.gd
+++ scripts/buildings/gathering_buildings/wood_cutter.gd
@@ -19,36 +19,36 @@ func check_for_output_overflow() -> bool:
 	## Maybe for future, adjust so that if that were the case then the
 	## resources to be gathered within the gathering area are selected to a few.
 	## Still allowing to get closer to max storage, without overflow
-	for produced_good in produced_goods:
-		var gather_rate_per_tile: int = output_generation.get(produced_good)
-		var gather_dict: Dictionary[Vector2, int] = _gather_area(gather_rate_per_tile)
+	var wood_gather_rate_per_tile: int = output_generation.get(Enums.ResourceType.WOOD)
+	var gather_dict: Dictionary[Vector2, int] = _gather_area(wood_gather_rate_per_tile)
+	
+	var total_wood_gathered: int = 0
+	for wood_gathered in gather_dict.values():
+		total_wood_gathered += wood_gathered
 		
-		var total_produced_goods_gathered: int = 0
-		for value in gather_dict.values():
-			total_produced_goods_gathered += value
+	var biomass_generated_rate: int = output_generation.get(Enums.ResourceType.BIOMASS)
+	var total_biomass_generated: int = total_wood_gathered / biomass_generated_rate
+	
+	var wood_stored: int = output_storage.get(Enums.ResourceType.WOOD)
+	var wood_max_storage: int = max_storage.get(Enums.ResourceType.WOOD)
+	
+	var biomass_stored: int = output_storage.get(Enums.ResourceType.BIOMASS)
+	var biomass_max_storage: int = max_storage.get(Enums.ResourceType.BIOMASS)
 
-		var produced_good_stored: int = output_storage.get(produced_good)
-		var produced_good_max_storage: int = max_storage.get(produced_good)
-		var produced_good_string = Enums.resource_type_to_string(produced_good)
-		
-		# print(produced_good_string + " to be generated: " + str(total_produced_goods_gathered))
-		# print("Current " + produced_good_string + " stored: " + str(produced_good_stored))
-		# print("Max storage: " + str(produced_good_max_storage))
-		
-		## When at possible overflow of resources for output
-		if produced_good_stored + total_produced_goods_gathered > produced_good_max_storage:
-			return true
+	## When at possible overflow of resources for output
+	if wood_stored + total_wood_gathered > wood_max_storage or biomass_stored + total_biomass_generated > biomass_max_storage:
+		return true
 	return false
 			
 ## Function to produce the goods the gathering building can output.
+## TODO: probably make it so wood is gathered in the center
 func _produce_goods() -> void:
 	for produced_good in produced_goods:
 		var gather_rate_per_tile: int = output_generation.get(produced_good)
+		var gather_dict: Dictionary[Vector2, int] = _gather_area(gather_rate_per_tile)
 		var produced_good_stored: int = output_storage.get(produced_good)
 		var produced_good_generated: int = 0
-		
-		var gather_dict: Dictionary[Vector2, int] = _gather_area(gather_rate_per_tile)
-		
+
 		for resource_pos in resource_tiles_to_gather.keys():
 			if is_instance_valid(resource_tiles_to_gather.get(resource_pos)):
 				var amount_to_gather: int = gather_dict.get(resource_pos)
@@ -77,8 +77,11 @@ func _generate_byproducts() -> void:
 		var byproduct_generated_rate: int = output_generation.get(byproduct)
 		var byproduct_stored: int = output_storage.get(byproduct)
 		var byproduct_generated: int = produced_wood / byproduct_generated_rate
-				
+		## TODO: 
+		byproduct_generated = max(byproduct_generated, 1)
+		
 		ResourceSignals.add_resource.emit(byproduct, byproduct_generated, self)
+		
 		if Enums.is_emission(byproduct):
 			emitted_emissions.emit(self, byproduct, byproduct_generated)
 		else:
@@ -95,6 +98,7 @@ func _gather_area(amount: int) -> Dictionary[Vector2, int]:
 	for x in range(-gather_radius, gather_radius + 1):
 		for y in range(-gather_radius, gather_radius + 1):
 			var tile_pos = position + Vector2(x * grid_size, y * grid_size)
+			
 			## Manhattan distance, grid based
 			var distance_from_building: int = abs(x) + abs(y)
 			
@@ -102,6 +106,6 @@ func _gather_area(amount: int) -> Dictionary[Vector2, int]:
 			var amount_to_set: int = amount * (1 - distance_from_building / gather_radius)
 			
 			## Non zero and no negative values
-			amount_to_set = max(amount_to_set, 1)
+			amount_to_set = max(amount_to_set, 2)
 			gather_dict.set(tile_pos, amount_to_set)
 	return gather_dict
