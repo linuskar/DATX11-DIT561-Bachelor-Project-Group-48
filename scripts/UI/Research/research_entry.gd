ad904e7d34aafb51bd417d8863940cfdeeafaabd
Kevin Faghihi <kevinfaghihi@hotmail.com>
2025-04-14 15:43:35 +0200

    Allt UI relaterat klart

---
+++ scripts/UI/Research/research_entry.gd
@@ -0,0 +1,27 @@
+extends Button
+class_name ResearchEntry
+
+@export var research_data: Resource
+
+@onready var name_label = $HBoxContainer/LeftContainer/VBoxContainer/Name
+@onready var description_label = $HBoxContainer/LeftContainer/VBoxContainer/Description
+@onready var cost_label = $HBoxContainer/RightContainer/Cost
+
+func _ready() -> void:
+	if research_data:
+		name_label.text = research_data.research_name
+		description_label.text = research_data.description
+		cost_label.text = "Cost: FREE"  # Placeholder
+
+		if Research.has_completed(research_data.research_id):
+			disabled = true
+
+		connect("pressed", _on_pressed)
+
+
+func _on_pressed() -> void:
+	if Research.has_completed(research_data.research_id):
+		return
+
+	Research.complete_research(research_data.research_id)
+	disabled = true
