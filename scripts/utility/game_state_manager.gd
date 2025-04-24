da52365d02235579df9e42853f565c2473fc3b29
Heldereim <adam.hellgard@hotmail.se>
2025-04-24 12:36:22 +0200

    Pause toggleable, exit game button
    Made the pause key toggleable, also added a button in the pause menu for quitting the game

--- scripts/utility/game_state_manager.gd
+++ scripts/utility/game_state_manager.gd
@@ -4,12 +4,17 @@ extends Node
 ## of the game updates.
 signal game_state_updated(paused: bool)
 
+## Set the script to always update even if the rest of 
+## the game is paused.
+func _ready() -> void:
+	process_mode = Node.PROCESS_MODE_ALWAYS
+
 ## If the 'pause game' button is pressed halt all processes
 ## default button is tab
 func _input(event: InputEvent) -> void:
 	if Input.is_action_just_pressed("pause game"):
-		game_state_updated.emit(true)
-		get_tree().paused = true
+		game_state_updated.emit(not get_tree().paused)
+		get_tree().paused = not get_tree().paused
 
 func _resume_game() -> void:
 	game_state_updated.emit(false)
