c46d2d56cce45be8b82659c6e9f7180b5dce3597
Heldereim <adam.hellgard@hotmail.se>
2025-04-24 14:33:17 +0200

    Pause button to game header

--- scripts/utility/game_state_manager.gd
+++ scripts/utility/game_state_manager.gd
@@ -8,14 +8,19 @@ signal game_state_updated(paused: bool)
 ## the game is paused.
 func _ready() -> void:
 	process_mode = Node.PROCESS_MODE_ALWAYS
+	game_state_updated.connect(update_game_state)
 
 ## If the 'pause game' button is pressed halt all processes
 ## default button is tab
 func _input(event: InputEvent) -> void:
 	if Input.is_action_just_pressed("pause game"):
 		game_state_updated.emit(not get_tree().paused)
-		get_tree().paused = not get_tree().paused
 
+## Updates the state of the game to the given variable
+func update_game_state(paused: bool) -> void:
+	get_tree().paused = paused
+
+## Unpauses the game
 func _resume_game() -> void:
 	game_state_updated.emit(false)
 	get_tree().paused = false
