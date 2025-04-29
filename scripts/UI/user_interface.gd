707934233e8e4a4909ddc10038a5e7f4dafceeda
Heldereim <adam.hellgard@hotmail.se>
2025-04-29 15:08:33 +0200

    Selecting buildings fix
    Fixed issues with selecting buildings and hindering placement of buildings when mouse is in a menu.

--- scripts/UI/user_interface.gd
+++ scripts/UI/user_interface.gd
@@ -1,10 +1,6 @@
 class_name UserInterface
 extends CanvasLayer
 
-signal build_list_entered
-
-signal ui_status(status: bool)
-
 signal building_wanted(building: BuildingData)
 
 static var hovered_references: Dictionary[UIElement, int] = {}
@@ -12,11 +8,7 @@ static var hovered_references: Dictionary[UIElement, int] = {}
 @onready var panel_container: UIElement = $PanelContainer
 
 func _process(delta: float) -> void:
-	ui_status.emit(not hovered_references.is_empty())
 	ZoomHandler.allow_zoom.emit(hovered_references.is_empty())
-
-func _on_building_select_list_build_list_entered() -> void:
-	build_list_entered.emit() 
 
 func _on_building_select_list_building_wanted(building: BuildingData) -> void:
 	building_wanted.emit(building)
