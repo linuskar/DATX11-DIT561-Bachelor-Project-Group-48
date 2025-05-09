d2a67372dc0757033689edad33b34410866ebc1d
Heldereim <adam.hellgard@hotmail.se>
2025-05-09 13:11:47 +0200

    Split selectables into categories
    Split the selectables contained in building select list into four categories: Gathering, Producing, Storing, Utility

--- scripts/utility/ui_menu.gd
+++ scripts/utility/ui_menu.gd
@@ -1,5 +1,8 @@
 class_name UIMenu extends UIElement
 
+@onready var buildings: PanelContainer = $ScrollContainer/BuildingsContainer
+@onready var tabs: TabBar = $TabBar
+
 func _ready() -> void:
 	super()
 	visibility_changed.connect(_on_menu_visibility_changed)
@@ -14,3 +17,9 @@ func _on_menu_visibility_changed() -> void:
 ## Can be overriden by classes that extend this.
 func hide_ui_menu() -> void:
 	hide()
+
+## Function for setting the corresponding list visible
+func _set_tab_visible(tab_num: int) -> void:
+	for child in buildings.get_children():
+		child.hide()
+	buildings.get_child(tab_num).show()
